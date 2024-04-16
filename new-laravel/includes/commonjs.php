d3d2939c57f9e904f851a7b31080f8127a117ddf
Adil Ahmed Khan <112798933+AdilAhmedAA@users.noreply.github.com>
2024-04-16 08:57:22 -0700

    updates

--- new-laravel/includes/commonjs.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript" src="<?= $baseurl ?>js/commonJs.js"></script>
-<script type="text/javascript" src="<?= $baseurl ?>js/lightslider.min.js"></script>
-<script type="text/javascript" src="<?= $baseurl ?>js/main.js?v=1;"></script>
-
